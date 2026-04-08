6abaaf0cfff859be33b1b38686e7674ddb8a7010
Sean McCreary <mccreary@mcwest.org>
2026-04-08 10:10:06 -0600

    drivers/input: bbqX0kbd: Don't require NUMLOCK for symbols
    Generate numbers and symbols whenever KEY_LEFTALT is down, not
    just when NUMS_LOCK_BIT is active.
    
    Change-Id: Idf2fca58f6a3884024d172248ceda2bcc7072c4a

--- drivers/input/keyboard/bbqX0kbd/bbqX0kbd_main.c
+++ drivers/input/keyboard/bbqX0kbd/bbqX0kbd_main.c
@@ -447,7 +447,7 @@ static void bbqX0kbd_work_handler(struct work_struct *work_struct)
 						bbqX0kbd_data->modifier_keys_status &= ~bbqX0kbd_modkeys_to_bits(keycode);
 					fallthrough;
 				default:
-					if (bbqX0kbd_data->lockStatus & NUMS_LOCK_BIT)
+					if (bbqX0kbd_data->lockStatus & (NUMS_LOCK_BIT | LEFT_ALT_BIT))
 						keycode = bbqX0kbd_get_num_lock_keycode(keycode);
 					else if (bbqX0kbd_data->modifier_keys_status & RIGHT_ALT_BIT)
 						keycode = bbqX0kbd_get_altgr_keycode(keycode);
@@ -598,7 +598,7 @@ static void bbqX0kbd_work_fnc(struct work_struct *work_struct_ptr)
 					bbqX0kbd_data->modifier_keys_status &= ~bbqX0kbd_modkeys_to_bits(keycode);
 				fallthrough;
 			default:
-				if (bbqX0kbd_data->lockStatus & NUMS_LOCK_BIT)
+				if (bbqX0kbd_data->lockStatus & (NUMS_LOCK_BIT | LEFT_ALT_BIT))
 					keycode = bbqX0kbd_get_num_lock_keycode(keycode);
 				else if (bbqX0kbd_data->modifier_keys_status & RIGHT_ALT_BIT)
 					keycode = bbqX0kbd_get_altgr_keycode(keycode);
